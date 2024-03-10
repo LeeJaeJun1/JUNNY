cfef1bab7f57de29d5bbe6b74430ba498a4ef814
LeeJaeJun1 <108278044+LeeJaeJun1@users.noreply.github.com>
2024-03-10 15:41:39 +0900

    [level 2] Title: 전화번호 목록, Time: 1060.68 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,16 +1,20 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
-    sort(phone_book.begin(), phone_book.end());
-    for(int i = 0; i < phone_book.size() - 1; i++) {
-        if(phone_book[i] == phone_book[i+1].substr(0, phone_book[i].size())) {
-            return false;
+    map<string,int> m;
+    for(int i = 0; i < phone_book.size(); ++i) {
+        m[phone_book[i]]++;
+    }
+    
+    for(int i = 0; i < phone_book.size(); i++) {
+        for (int j = 1; j < phone_book[i].size(); j++) {
+            if(m[phone_book[i].substr(0,j)]) return false;
         }
     }
-    return answer;
+    
+    return true;
 }
