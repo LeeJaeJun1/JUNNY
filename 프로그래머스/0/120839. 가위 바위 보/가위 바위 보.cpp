e25bec8568b7a1bb2293dad7935d7c75eb49f270
LeeJaeJun1 <108278044+LeeJaeJun1@users.noreply.github.com>
2024-03-26 22:21:07 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for(int i = 0; i < rsp.length(); i++) {
+        if(rsp[i] == '2') {
+            answer += '0';
+        }
+        else if(rsp[i] == '0') {
+            answer += '5';
+        }
+        else if(rsp[i] == '5') {
+            answer += '2';
+        }
+    }
+    return answer;
+}
